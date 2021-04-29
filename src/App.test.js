cae2e2bf47864fb20bacd31ebac5b196badf33ab
Valentyn Mirosh <vlnt@ua.fm>
2021-04-29 19:11:10 +0300

    test file repaired

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(s/This React app is running in a Docker container/i);
   expect(linkElement).toBeInTheDocument();
 });
